57cf3ad484fc4197f7a25de768ab53f278d21530
OmitsuSama <lattiassia@gmail.com>
2022-09-11 19:03:59 +0200

    API fin

--- api.py
+++ api.py
@@ -15,3 +15,37 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Bienvenue, L'API est opérationnelle..."
+
+@app.route('/prediction_credit/<id_client>')  #, methods=['GET'])
+def prediction_credit(id_client):
+    print('id client = ', id_client)
+    
+    ID = int(id_client)
+    X = data[data['SK_ID_CURR'] == ID]
+    
+    print("Laisse moi tranquille : ", X)
+
+    notimportant_features = ['SK_ID_CURR', 'INDEX', 'TARGET']
+    selected_features = [col for col in data.columns if col not in notimportant_features]
+    
+    X = X[selected_features]
+    
+    print('La taille du vecteur X  = ', X.shape)
+    
+    proba = model.predict_proba(X)
+    prediction = model.predict(X)
+ 
+    print('L\'identificateur du client : ', id_client)
+  
+    dict_final = {
+        'prediction' : int(prediction),
+        'proba' : float(proba[0][0])
+        }
+   
+    print('Lancer une nouvelle Prédiction : \n', dict_final)
+            
+    return jsonify(dict_final)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
