9f902078e88c5837e12a5f41b862ed671311d06b
OmitsuSama <lattiassia@gmail.com>
2022-09-11 19:02:25 +0200

    chargement du modèle

--- api.py
+++ api.py
@@ -7,3 +7,11 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 data = pd.read_csv('app_test.csv')
 print('la taille de Dataframe est = ', data.shape)
+
+model = joblib.load(open('Model.joblib', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Bienvenue, L'API est opérationnelle..."
